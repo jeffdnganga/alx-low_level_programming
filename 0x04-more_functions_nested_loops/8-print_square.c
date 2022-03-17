b19afa1c3c9cf13564c65a852696de5a18d8b8f3
jeffdnganga <jeffdaniels950@gmail.com>
2022-03-17 16:10:36 -0700

    a function that draws a diagonal line on the terminal.

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,7 @@ void print_square(int size)
 {
 	if (size <= 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 	} else
 	{
 		int i, j;
@@ -17,9 +17,9 @@ void print_square(int size)
 		{
 			for (j = 0; j < size; j++)
 			{
-				putchar('#');
+				_putchar('#');
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
